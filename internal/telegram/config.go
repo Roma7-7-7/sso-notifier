f61ed292752aa7febf7fba710efcfa0b7e17aace
Roman Savchyshyn <roma.fln137@gmail.com>
2025-10-30 12:14:06 +0200

    feat: support ssm for telegram token (#12)
    * feat: support ssm for telegram token
    
    * fix: lint

---
+++ internal/telegram/config.go
@@ -0,0 +1,66 @@
+package telegram
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Dev                      bool          `envconfig:"DEV" default:"false"`
+	GroupsCount              int           `envconfig:"GROUPS_COUNT" default:"12"`
+	DBPath                   string        `envconfig:"DB_PATH" default:"data/sso-notifier.db"`
+	RefreshShutdownsInterval time.Duration `envconfig:"REFRESH_SHUTDOWNS_INTERVAL" default:"5m"`
+	NotifyInterval           time.Duration `envconfig:"NOTIFY_INTERVAL" default:"5m"`
+	TelegramToken            string        `envconfig:"TELEGRAM_TOKEN"`
+}
+
+func NewConfig(ctx context.Context) (*Config, error) {
+	res := &Config{}
+
+	err := envconfig.Process("", res)
+	if err != nil {
+		return nil, fmt.Errorf("envconfig process: %w", err)
+	}
+
+	if res.Dev {
+		return res, nil
+	}
+	res.TelegramToken, err = getSSMToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.TelegramToken == "" {
+		return nil, errors.New("telegram token is required")
+	}
+
+	return res, nil
+}
+
+func getSSMToken(ctx context.Context) (string, error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return "", fmt.Errorf("load aws config: %w", err)
+	}
+	ssmClient := ssm.NewFromConfig(cfg)
+
+	param, err := ssmClient.GetParameter(ctx, &ssm.GetParameterInput{
+		Name:           aws.String("/sso-notifier-bot/prod/telegram-token"),
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		return "", fmt.Errorf("get SSM token: %w", err)
+	}
+	if param.Parameter.Value == nil {
+		return "", errors.New("SSM Token not found")
+	}
+
+	return *param.Parameter.Value, nil
+}
